b39ec41c0097f8618678e9552d8877872974f6e7
kin280 <74567462+kin280@users.noreply.github.com>
2024-05-31 13:43:37 +0700

    TemRun
    TemRun

---
+++ Assets/Scripts/Tiles/Gem.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            PlayerPrefs.SetInt("TotalGems", PlayerPrefs.GetInt("TotalGems", 0) + 1);
+            FindObjectOfType<AudioManager>().PlaySound("PickUp");
+            PlayerManager.score += 2;
+            gameObject.SetActive(false);
+        }
+    }
+}
